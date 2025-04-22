a1be4c67f52bd31bf0f559725685598f310e6701
ph-kiryu <prettyhater@gmail.com>
2025-04-22 13:38:59 +0200

    Create telegram_notify.py

---
+++ telegram_notify.py
@@ -0,0 +1,17 @@
+import requests
+import os
+
+def send_telegram(message):
+    token = os.getenv("TELEGRAM_TOKEN")
+    chat_id = os.getenv("TELEGRAM_CHAT_ID")
+    if not token or not chat_id:
+        print("Telegram credentials not found.")
+        return
+
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
+    payload = {
+        "chat_id": chat_id,
+        "text": message
+    }
+    response = requests.post(url, data=payload)
+    print("Telegram message sent." if response.status_code == 200 else "Telegram error.")
